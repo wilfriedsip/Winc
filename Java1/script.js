d4c4ca5e6accce8b7398853a2873f29544999597
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-11 21:49:24 +0100

    Update script.js

--- Java1/script.js
+++ Java1/script.js
@@ -4,19 +4,19 @@ console.log(name);
 //if is  reserved 
 
 let Addition = 1 + 2;
-console.log("Addition:");
+console.log(Addition);
 
 let subtraction = 7 - 2;
-console.log("subtraction:");
+console.log(subtraction);
 
 let multiplication = 25 * 25;
-console.log("multiplication:");
+console.log(multiplication);
 
 let division = 6 / 3;
-console.log("division:");
+console.log(division);
 
 let modulus = 50 % 19;
-console.log("modulus:");
+console.log(modulus);
 
 let age = 44;
 console.log(typeof age);
