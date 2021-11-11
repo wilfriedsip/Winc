c953cd3a7aeb6a1508614d39fc0c80692da9f191
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-11 21:32:25 +0100

    Update script.js

--- Java1/script.js
+++ Java1/script.js
@@ -1,9 +1,24 @@
 console.log('Hello Winc Academy')
+let name = 'wilfried';
+console.log(name);
+//if is  reserved 
 
-let Name = 'wilfried';
-console.log(Name);
+let sum = 1 + 2;
+console.log("sum:", sum);
+
+let subtraction = 7 - 2;
+console.log("subtraction:", subtraction);
+
+let multiplication = 25 * 25;
+console.log("multiplication:", multiplication);
+
+let division = 6 / 3;
+console.log("division:", division);
+
+let modulus = 50 % 19;
+console.log("modulus:", modulus);
 
 let age = 44;
-console.log(age);
-
-"2 + 2"
+console.log(typeof age);
+let ageAsString = "48";
+console.log(typeof ageAsString);
