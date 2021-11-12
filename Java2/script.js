aad305f38f53818eccfa8bbb5592f7fccfa15a95
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-12 10:29:07 +0100

    Add files via upload

---
+++ Java2/script.js
@@ -0,0 +1,12 @@
+console.log('Hello Winc Academy Students')
+/*
+//This is a very large sum
+let Multiplication = 1230941 * 1823794;
+console.log(Multiplication);
+
+let Division = 637263 / 54;
+console.log(Division)
+
+//4. I see it because it just info
+
+*/
