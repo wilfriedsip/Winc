d54656eb2dbf6507880474fd9a882bb322b2b8f6
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-12 10:31:06 +0100

    Update script.js

--- Java2/script.js
+++ Java2/script.js
@@ -5,7 +5,7 @@ let Multiplication = 1230941 * 1823794;
 console.log(Multiplication);
 
 let Division = 637263 / 54;
-console.log(Division)
+console.log(Division);
 
 //4. I see it because it just info
 
