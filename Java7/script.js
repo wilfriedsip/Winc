5b79f5d81c3cbe611e92cd99791590da78033057
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-14 11:46:28 +0100

    Add files via upload

---
+++ Java7/script.js
@@ -0,0 +1,19 @@
+
+
+const paintWall = function (color, wall) {
+
+    console.log("The wall has been painted " + color);
+    console.log("The north wall has been painted " + wall);
+    console.log("The south-east wall has been painted" + wall);
+};
+
+const color1 = "Green";
+const color2 = "Yellow";
+const northWall = "Orange";
+const southWall = "Grey";
+
+
+paintWall(color2);
+paintWall(northWall);
+
+
