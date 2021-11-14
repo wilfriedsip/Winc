8ab9507c1de347a774ffb5f0799463a8142d82a1
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-14 09:54:22 +0100

    Add files via upload

---
+++ Java6/script.js
@@ -0,0 +1,36 @@
+const makePizza = function () {
+
+    console.log("Step 1: Ingredients");
+    console.log("Step 2: Bring Out the Yeast!");
+    console.log("Step 3: Add Some More Stuff..");
+    console.log("Step 4: Toss and Turn...");
+    console.log("Step 5: Take Cover!");
+    console.log("Step 6: Punch!");
+    console.log("Step 7: Be Fair!");
+    console.log("Step 8: Take Cover Pt. 2");
+    console.log("Step 9: Ingredients Pt 2");
+    console.log("Step 10: Forming the Pizza");
+    console.log("Step 11: Bake!");
+    console.log("Step 12: Remove and Top");
+    console.log("Step 13: Bake Pt 2");
+    console.log("Step 14: Enjoy!");
+};
+
+//7. No result, We write the function, didn't active it.
+
+const makeSushi = function () {
+
+    console.log("Step 1: Prepare the Japanese sushi rice");
+    console.log("Step 2: Cover bamboo mat with plastic wrap.");
+    console.log("Step 3: Cut nori sheet in half.");
+    console.log("Step 4: Flip the nori over (rice side facing down)");
+    console.log("Step 5: Grab the nori and the rolling mat.");
+    console.log("Step 6: Transfer to a cutting board.");
+    console.log("Step 7: Serve immediately with wasabi and pickled ginger.");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
