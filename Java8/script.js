be31ffbc8da6c2fbb120ad6521eb747935959356
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-15 16:33:13 +0100

    Add files via upload

---
+++ Java8/script.js
@@ -0,0 +1,25 @@
+const biggerNumber = function (bigger) {
+
+    if (bigger > 100) {
+        console.log("True")
+    } else {
+        console.log("False")
+    }
+};
+biggerNumber(101);
+
+//Bouncer at a club
+
+const aiBouncer = function (amount, age) {
+    console.log("come in");
+    console.log("it's too busy now, come back later");
+    console.log("this is a club for adults");
+}
+
+amount();
+age("");
+
+//Calculating the average
+
+
+
