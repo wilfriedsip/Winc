be6931b35dcf878521ea310764c78297773daf1a
wilfriedsip <60424738+wilfriedsip@users.noreply.github.com>
2021-11-15 16:51:20 +0100

    Update script.js

--- Java8/script.js
+++ Java8/script.js
@@ -25,3 +25,14 @@ const aiBouncer = function (amountVisitors, ageVisitors) {
 console.log(aiBouncer(400, 0));
 console.log(aiBouncer(400, 19));
 console.log(aiBouncer(1400, 19));
+
+//Calculating the average
+
+const calAverage = function (number1, number2, number3, number4, number5) {
+
+    const total = number1 + number2 + number3 + number4 + number5;
+    const average = total / 5;
+    return average;
+};
+
+console.log(calAverage(10, 14, 21, 100, 50));
